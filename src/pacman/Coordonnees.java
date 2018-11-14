3cfb88fe0b30c0e4dc2b1c4a78041d443c7b6ae5
tblain <tblain69@gmail.com>
2018-11-14 15:36:26 +0100

    ajout classe Coordonnes

---
+++ src/pacman/Coordonnees.java
@@ -0,0 +1,27 @@
+package pacman;
+
+public class Coordonnees {
+    public int x;
+    public int y;
+
+    public Coordonnees(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
