b1cb0ed3175271cb049fe62d6cd0fd4933c42404
Mathéo Dumont <math.dumont.md@gmail.com>
2018-11-14 11:46:24 +0100

    pas add au commit précédent

--- src/Calculette/VariableInt.java
+++ src/Calculette/VariableInt.java
@@ -1,4 +1,14 @@
 package Calculette;
 
 public class VariableInt {
+
+    public int mValue;
+
+    int GetValue() {
+        return mValue;
+    }
+
+    void SetValue(int inValue) {
+        mValue=inValue;
+    }
 }
